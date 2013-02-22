5af48c5cc742ee681cc3c0179376ea73c3d725f5
Theoxylo <theoxylo@yahoo.com>
2013-02-22 14:16:27 -0500

    removed semicolons for consistency with voxel-* coding style

--- demo.js
+++ demo.js
@@ -1,47 +1,47 @@
-var createGame = require('voxel-engine');
-var texturePath = require('painterly-textures')(__dirname);
-var highlighter = require('./');
+var createGame = require('voxel-engine')
+var texturePath = require('painterly-textures')(__dirname)
+var highlighter = require('./')
 
-var container = document.querySelector('#container');
+var container = document.querySelector('#container')
 
 var game = createGame({
   startingPosition: [0, 1000, 0],
   texturePath: texturePath
-});
+})
 
-game.controls.pitchObject.rotation.x = -1.5; // look down
-game.appendTo(container);
-game.currentMaterial = 1;
+game.controls.pitchObject.rotation.x = -1.5 // look down
+game.appendTo(container)
+game.currentMaterial = 1
 
 var highlight = highlighter(game, {
   distance: 100,
   wireframeLinewidth: 10,
-});
+})
 
 highlight.on('highlight', function(position, mesh, vidx) {
-  console.log('highlighted: ' + vidx);
-});
+  console.log('highlighted voxel: ' + vidx)
+})
 
 highlight.on('remove', function(mesh, vidx) {
-  console.log('# UN-highlighted ' + vidx);
-});
+  console.log('UN-highlighted voxel: ' + vidx)
+})
 
 game.on('mousedown', function (pos) {
-  if (game.erase) game.setBlock(pos, 0);
-  else game.createBlock(pos, game.currentMaterial);
-});
+  if (game.erase) game.setBlock(pos, 0)
+  else game.createBlock(pos, game.currentMaterial)
+})
 
 container.addEventListener('click', function() {
-  game.requestPointerLock(container);
-});
+  game.requestPointerLock(container)
+})
 
-game.erase = true;
+game.erase = true
 window.addEventListener('keydown', function (ev) {
   if (ev.keyCode === 'X'.charCodeAt(0)) {
-    game.erase = !game.erase;
+    game.erase = !game.erase
   }
-});
+})
 
 function ctrlToggle (ev) { game.erase = !ev.ctrlKey }
-window.addEventListener('keyup', ctrlToggle);
-window.addEventListener('keydown', ctrlToggle);
+window.addEventListener('keyup', ctrlToggle)
+window.addEventListener('keydown', ctrlToggle)
