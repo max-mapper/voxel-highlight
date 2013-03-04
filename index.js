ae4343e2821ae55f916421b0dfff7269873e0e90
Theoxylo <theoxylo@yahoo.com>
2013-03-04 04:22:08 -0500

    changed to unix lf

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function Highlighter(game, opts) {
   if (!(this instanceof Highlighter)) return new Highlighter(game, opts)
   this.game = game
   opts = opts || {}
-  this.currVoxelIdx // undefined when no voxel selected for highlight
+  this.currVoxelPos // undefined when no voxel selected for highlight
   var geometry = opts.geometry || new game.THREE.CubeGeometry(1, 1, 1)
   var material = opts.material || new game.THREE.MeshBasicMaterial({
     color: opts.color || 0x000000,
@@ -29,29 +29,30 @@ Highlighter.prototype.highlight = function () {
   var cv = this.game.cameraVector()
   var hit = this.game.raycastVoxels(cp, cv, this.distance)
   if (!hit) {
-    if (this.currVoxelIdx) { // remove existing highlight
+    if (this.currVoxelPos) { // remove existing highlight
       this.game.scene.remove(this.mesh)
-      this.emit('remove', this.mesh, this.currVoxelIdx)
-      this.currVoxelIdx = undefined // can't use 0 since there could be a voxel there
+      this.emit('remove', this.mesh, this.currVoxelPos)
+      this.currVoxelPos = undefined // can't use 0 since there could be a voxel there
     }
     return // no highlight, done with common case
   }
   hit.position[0] = Math.floor(hit.position[0]) + 0.5
   hit.position[1] = Math.floor(hit.position[1]) + 0.5
   hit.position[2] = Math.floor(hit.position[2]) + 0.5
-  
-  var newVoxelIdx = hit.position.join("|")
-  if (newVoxelIdx === this.currVoxelIdx) {
+
+  //var newVoxelPos = this.game.blockPosition(hit.position) // coming soon
+  var newVoxelPos = hit.position.join("|")
+  if (newVoxelPos === this.currVoxelPos) {
     return // voxel already highlighted, done with common case
   }
   this.mesh.position.set(hit.position[0], hit.position[1], hit.position[2])
 
-  if (this.currVoxelIdx) {
-    this.emit('remove', this.mesh, this.currVoxelIdx) // moved highlight
+  if (this.currVoxelPos) {
+    this.emit('remove', this.mesh, this.currVoxelPos) // moved highlight
   }
   else {
     this.game.scene.add(this.mesh) // fresh highlight
   }
-  this.emit('highlight', this.mesh, newVoxelIdx)
-  this.currVoxelIdx = newVoxelIdx
+  this.emit('highlight', this.mesh, newVoxelPos)
+  this.currVoxelPos = newVoxelPos
 }
